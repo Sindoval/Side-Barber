b0a7ca3c9923dec4fe72279afa16b5c2e76b5264
Sindoval <sindoval321@gmail.com>
2025-07-28 22:38:12 -0300

    feat: add Booking-item component

---
+++ side-barber/src/app/_constants/search.ts
@@ -0,0 +1,25 @@
+interface QuickSearchOption {
+  imageUrl: string;
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [{
+  imageUrl: "/cabelo.svg",
+  title: "Cabelo"
+}, {
+  imageUrl: "/barba.svg",
+  title: "Barba"
+}, {
+  imageUrl: "/navalha.svg",
+  title: "Acabamento"
+}, {
+  imageUrl: "/toalha.svg",
+  title: "Massagem"
+}, {
+  imageUrl: "/sobrancelha.svg",
+  title: "Sobrancelha"
+}, {
+  imageUrl: "/creme.svg",
+  title: "Hidratação"
+}
+]
