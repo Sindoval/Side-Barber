afe8476eb46dd041089d97a36912696ea308f5f8
Sindoval <sindoval321@gmail.com>
2025-08-01 18:46:08 -0300

    chore: add next-auth setup

--- side-barber/src/app/_components/sidebar-sheet.tsx
+++ side-barber/src/app/_components/sidebar-sheet.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { SheetClose, SheetContent, SheetTitle } from "./ui/sheet";
 import { quickSearchOptions } from "../_constants/search";
-import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 
 const SidebarButton = () => {
@@ -13,15 +13,39 @@ const SidebarButton = () => {
     <SheetContent>
       <SheetTitle>Menu</SheetTitle>
 
-      <div className="py-5 border-b flex items-center gap-3 border-solid">
-        <Avatar>
+      <div className="py-5 border-b flex items-center gap-3 border-solid justify-between">
+        <h2 className="font-bold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="w-[90%]">
+            <DialogHeader>
+              <DialogTitle>Faça seu login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button className="flex gap-1 font-bold" variant="outline">
+              <Image
+                src="/Google.svg"
+                height={18}
+                width={18}
+                alt="Fazer login com Google" />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+        {/*  <Avatar>
           <AvatarImage src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
         </Avatar>
 
         <div>
           <p className="font-bold">Sindoval Alves</p>
           <p className="text-gray-400 text-xs">sindoval@email.com</p>
-        </div>
+        </div> */}
       </div>
 
       <div className="flex flex-col gap-2 border-b border-solid py-5">
